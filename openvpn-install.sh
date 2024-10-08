1944ec09c7accb37448e4514f35470f95ada2b57
zjt <67032173+zjt000@users.noreply.github.com>
2024-10-08 15:59:55 +0800

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# https://github.com/Nyr/openvpn-install
+# https://github.com/zjt000/openvpn-install
 #
-# Copyright (c) 2013 Nyr. Released under the MIT License.
+# Copyright (c) 2024 zjt000. Released under the MIT License.
 
 
 # Detect Debian users running the script with "sh" instead of bash
